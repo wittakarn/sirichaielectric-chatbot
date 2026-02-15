c0db45b2283d2ea4a7591424da94af7f92ed5851
wittakarn keeratichayakorn <wittakarn.expert@gmail.com>
2026-02-15 13:30:10 +0700

    Fix missing log

--- test-chatbot.php
+++ test-chatbot.php
@@ -16,6 +16,7 @@
 // Set error reporting for debugging
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
+ini_set('error_log', __DIR__ . '/logs.log');
 
 // Load dependencies
 require_once __DIR__ . '/config.php';
